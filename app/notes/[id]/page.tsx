46886f2e5a9dcccb0e678af26238ab039316918a
herdssman <matthew.hf@outlook.com>
2025-08-16 23:01:53 +0200

    adjust props

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,11 +2,15 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from '../NoteDetails.client';
 
+interface PageProps {
+	params: Promise<{ id: string }>
+}
 
-export default async function NoteDetails({ params }: { params: { id: string } }) {
+
+export default async function NoteDetails({ params }: PageProps) {
 
 	const queryClient = new QueryClient();
-	const id = params.id;
+	const { id } = await params;
 
 	await queryClient.prefetchQuery({
 		queryKey: ['note', id],
