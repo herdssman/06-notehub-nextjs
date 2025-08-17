d7a0081a14bc282892435d556a2d1907e35f4770
herdssman <matthew.hf@outlook.com>
2025-08-17 09:58:29 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import TanStackProvider from "@/components/TanStackProvider/anStackProvider";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <TanStackProvider>
-          <Hader/>
+          <Header/>
           {children}
           <Footer />
         </TanStackProvider>
